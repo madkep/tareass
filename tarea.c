f0eba532da75f2e45806b75f118cd63855f52e2b
madkep <felipecespedes97@gmail.com>
2017-11-18 18:31:29 -0300

    Tiene la primera wea

---
+++ tarea.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+
+int main(int argc, char const *argv[]) {
+  int maxweight = atoi(argv[1]);
+  printf("%d\n",maxweight);
+
+
+  return 0;
+}
